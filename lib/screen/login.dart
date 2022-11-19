1e2e3cd4f141594c31f5f58cb36cbcb6190b0224
ghaan1 <gono0608@gmail.com>
2022-11-20 03:07:09 +0700

    Login UI

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../api/http_helper.dart';
 
-
-
-
 class Login extends StatefulWidget {
   const Login({super.key});
 
@@ -14,38 +11,117 @@ class Login extends StatefulWidget {
 
 class _Login extends State<Login> {
 
-  final txtEmail = TextEditingController(text : 'superadmin@gmail.com');
-  final txtPassword = TextEditingController(text : 'password');
+  final txtEmail = TextEditingController();
+  final txtPassword = TextEditingController();
+  // final txtEmail = TextEditingController(text : 'superadmin@gmail.com');
+  // final txtPassword = TextEditingController(text : 'password');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(5),
+      resizeToAvoidBottomInset: false,
+      body: 
+      Container(
+        color: const Color(0xFFF3F6F9),
+        padding: const EdgeInsets.all(30),
+        //margin: EdgeInsets.all(8),
         child: Column(
-        children: [
-          TextFormField(
-            controller: txtEmail,
-            decoration: const InputDecoration(
-              hintText: 'Email'
-            ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:   [ 
+            const Text(
+              'Login',
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 24,
+                  color: Color(0xFF6777EE)
+                ),
+              ),
+          const SizedBox(
+            height: 10,
+          ),
+            TextFormField(
+              controller: txtEmail,
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 10,horizontal: 17),
+                filled: true,
+                fillColor: Colors.white,
+                labelText: 'Email',
+                labelStyle: const TextStyle(
+                  color: Color(0xFF6777EE),
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Nunito'
+                ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 3, 
+                      color: Color(0xFF6777EE)
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 3, 
+                      color: Color(0xFF6777EE)),
+                    borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+          ),     
+          const SizedBox(
+            height: 10,
           ),
           TextFormField(
             controller: txtPassword,
-            decoration: const InputDecoration(
-              hintText: 'Password'
-            ),
+            decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 10,horizontal: 17),
+                filled: true,
+                fillColor: Colors.white,
+                labelText: 'Password',
+                labelStyle: const TextStyle(
+                  color: Color(0xFF6777EE),
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Nunito'
+                ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 3, 
+                      color: Color(0xFF6777EE)
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 3, 
+                      color: Color(0xFF6777EE)),
+                    borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+          ),
+          const SizedBox(
+            height: 10,
           ),
           SizedBox(
+            height: 45,
             width: double.infinity,
             child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFF6777EE),
+                elevation: 10,
+                shape: RoundedRectangleBorder( //to set border radius to button
+                borderRadius: BorderRadius.circular(15)
+            ),
+              ),
+              
               onPressed: () {
                 doLogin();
               },
-              child: Text("Login")),
+              child: const Text("Login",
+              style: TextStyle(
+                  fontFamily: 'Nunito',
+                  fontWeight: FontWeight.bold
+                ))),
           ),
-        ],
-       ), 
+          ],
+        ),
       )
     );
   }
